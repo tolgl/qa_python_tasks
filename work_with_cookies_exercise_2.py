a39b236ff4009eb6641d1c128dad3a31338af837
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-20 12:29:47 +0300

    Изменение куки

--- work_with_cookies_exercise_2.py
+++ work_with_cookies_exercise_2.py
@@ -4,12 +4,18 @@ driver = webdriver.Chrome()
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
 # здесь добавь свой предыдущий код для добавления куки
-...
+new_cookie = {'name': 'my_first_cookie', 'value': '15'}
+driver.add_cookie(new_cookie)
 
 # а теперь измени значение куки
-...
+driver.delete_cookie('my_first_cookie')
+
+new_cookie = {'name': 'my_first_cookie', 'value': '25'}
+driver.add_cookie(new_cookie)
 
 # Проверь новое значение поля value для добавленной куки
-...
+cookie = driver.get_cookie('my_first_cookie')
+print(cookie['value'])
+assert cookie['value'] == '25'
 
 driver.quit()
