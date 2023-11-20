8bcb269284d4fcbcd91e90435652eae129488e90
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-20 10:57:16 +0300

    Добавление метода скролла до элемента

--- go_to_element.py
+++ go_to_element.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -7,24 +9,25 @@ driver = webdriver.Chrome()
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
 # Найди поле "Email" и заполни его
-...
+driver.find_element(By.ID, 'email').send_keys('glinkin@bigam.ru')
 
 # Найди поле "Пароль" и заполни его
-...
+driver.find_element(By.ID, 'password').send_keys('Veropharm1997!')
 
 # Найди кнопку "Войти" и кликни по ней
-...
+driver.find_element(By.CLASS_NAME, 'auth-form__button').click()
 
 # Добавь явное ожидание для загрузки страницы
-...
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'card__image')))
 
 # Найди футер
-element = ...
+element = driver.find_element(By.CLASS_NAME, 'footer__copyright')
 
 # Прокрути страницу до футера
-driver.execute_script(...)
+driver.execute_script("arguments[0].scrollIntoView();", element)
+
 
 # Проверь, что футер содержит текст 'Mesto Russia'
-assert ...
+assert 'Mesto Russia' in element.text
 
 driver.quit()
