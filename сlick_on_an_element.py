12e86ad4116f72c1a99e74153b755dc7627ba284
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-20 09:58:20 +0300

    Добавлено ожидание элемента

--- сlick_on_an_element.py
+++ сlick_on_an_element.py
@@ -1,13 +1,16 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 driver = webdriver.Chrome()
 
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
+# ожидаем пока элемент не станет активным
+WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, './/button[@class = "auth-form__button"]')))
 # Найди кнопку и кликни по ней
 driver.find_element(By.XPATH, './/button[@class = "auth-form__button"]').click()
-time.sleep(5)
+
 
 driver.quit()
