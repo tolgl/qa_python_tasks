813ca385dc7971c5d3e1e637cab28826cf9ce063
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-20 09:46:18 +0300

    Добавлен метод клика по кнопке Войти

--- сlick_on_an_element.py
+++ сlick_on_an_element.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
 # Найди кнопку и кликни по ней
-driver.find_element(...)...
+driver.find_element(By.XPATH, './/button[@class = "auth-form__button"]').click()
+time.sleep(5)
 
 driver.quit()
