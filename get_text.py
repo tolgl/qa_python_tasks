f1bfdef8ba0a852a27af9130778f48018d076675
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-20 10:37:36 +0300

    Метод получения текста элемента

--- get_text.py
+++ get_text.py
@@ -7,18 +7,18 @@ driver = webdriver.Chrome()
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
 # Найди поле "Email" и заполни его
-...
+driver.find_element(By.ID, 'email').send_keys('glinkin@bigam.ru')
 
 # Найди поле "Пароль" и заполни его
-...
+driver.find_element(By.ID, 'password').send_keys('Veropharm1997!')
 
 # Найди кнопку "Войти" и кликни по ней
-...
+driver.find_element(By.CLASS_NAME, 'auth-form__button').click()
 
 # Добавь явное ожидание для загрузки страницы
-WebDriverWait(...).until(...)
+WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'header__user')))
 
 # Найди кнопку, получи её текст и проверь, что он равен 'Выйти'
-assert ...
+assert driver.find_element(By.CLASS_NAME, 'header__logout').text == 'Выйти'
 
 driver.quit()
