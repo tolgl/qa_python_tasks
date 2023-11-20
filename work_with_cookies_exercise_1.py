17dd10b6c162348485459ac743f4062ffef80927
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-20 12:21:37 +0300

    Добавление новой куки

--- work_with_cookies_exercise_1.py
+++ work_with_cookies_exercise_1.py
@@ -4,10 +4,12 @@ driver = webdriver.Chrome()
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
 # напиши код для добавления куки
-...
+new_cookie = {'name': 'my_first_cookie', 'value': '15'}
+driver.add_cookie(new_cookie)
 
 # Проверь поле value для добавленной куки
-cookie = ...
-assert ...
+cookie = driver.get_cookie('my_first_cookie')
+
+assert cookie['value'] == '15'
 
 driver.quit()
